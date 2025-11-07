276f6687b1bf3d75dbfd9007a7388b1d17f14f3c
CryptoDoge <Samsfright@gmail.com>
2025-11-07 08:34:14 +0000

    Add Tailwind CSS configuration with custom colors

---
+++ staking-dapp/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'dark-blue': '#001F3F',
+        'light-blue': '#00BFFF'
+      }
+    }
+  }
+};
